a11501610db284fd2801620d02efccaace806385
neal1991 <neal1991@sina.com>
2017-04-28 15:54:40 +0800

    first initial the project :tada:

---
+++ vuex/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+    notes: [],
+    activeNote: {}
+}
+
+const mutations = {
+
+}
+
+export defualt new Vuex.Store({
+    state,
+    mutations
+})
