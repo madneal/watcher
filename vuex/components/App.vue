0176459ec2d6dfe8dbad19b564784a5e335b2ea4
neal1991 <neal1991@sina.com>
2017-04-30 17:54:05 +0800

    add more vues

---
+++ vuex/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <toolbar></toolbar>
+        <notes-list></notes-list>
+        <editor></editor>
+    </div>
+</template>
+
+<script>
+import Toopbar form './Toolbar.vue'
+import NotesList form './NotesList.vue'
+import Editor from './Editor.vue'
+
+export default {
+    components: {
+        Toolbar,
+        NotesList,
+        Editor
+    }
+}
+</script>
---
+++ vuex/components/Toolbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="toolbar">
+        <i @click="addNote" class="glyphicon glyphicon-plus"></i>>
+        <i @click="toogleFavorite" 
+            class="glyphicon glyphicon-start"
+            :class="{starred: activeNote.favorite}"></i>>
+        <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>>
+    </div>
+</template>
+
+<script>
+    import { addNote, deleteNote, toogleFavorite } from '../vuex/actions'
+    export default {
+        vuex: {
+            getters: {
+                activeNote: state => state.activeNote
+            },
+            actions: {
+                addNote,
+                deleteNote,
+                toggleFavorite
+            }
+        }
+    }
+</script>
