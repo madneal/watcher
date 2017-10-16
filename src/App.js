4933b9b1bfdba8e35e7d3c296a3d29bea2811c2b
neal1991 <neal1991@sina.com>
2017-10-16 22:07:32 +0800

    add class Header

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header></Header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+      </div>
+    )
   }
 }
 
